a63f43d27a130bd7979cc3000d304a6784337caf
Lukas Rytz <lukas.rytz@gmail.com>
2017-06-03 16:15:53 +0200

    wip

--- compilation/src/main/scala/scala/tools/nsc/ScalacBenchmark.scala
+++ compilation/src/main/scala/scala/tools/nsc/ScalacBenchmark.scala
@@ -26,8 +26,6 @@ class ScalacBenchmark {
   @Param(value = Array("latest"))
   var corpusVersion: String = _
 
-  var driver: Driver = _
-
   var depsClasspath: String = _
 
   def compileImpl(): Unit = {
@@ -42,34 +40,13 @@ class ScalacBenchmark {
         }).map(_.toAbsolutePath.normalize.toString).toArray
       }
 
-    // MainClass is copy-pasted from compiler for source compatibility with 2.10.x - 2.13.x
-    class MainClass extends Driver with EvalLoop {
-      def resident(compiler: Global): Unit = loop { line =>
-        val command = new CompilerCommand(line split "\\s+" toList, new Settings(scalacError))
-        compiler.reporter.reset()
-        new compiler.Run() compile command.files
-      }
+    val args = Array(
+      "-usejavacp",
+      "-d", tempDir.getAbsolutePath,
+      "-nowarn"
+    ) ++ compilerArgs
 
-      override def newCompiler(): Global = Global(settings, reporter)
-
-      override protected def processSettingsHook(): Boolean = {
-        if (source == "scala")
-          settings.sourcepath.value = Paths.get(s"../corpus/$source/$corpusVersion/library").toAbsolutePath.normalize.toString
-        else
-          settings.usejavacp.value = true
-        settings.outdir.value = tempDir.getAbsolutePath
-        settings.nowarn.value = true
-        if (depsClasspath != null)
-          settings.processArgumentString(s"-cp $depsClasspath")
-        if (extraArgs != null && extraArgs != "")
-          settings.processArgumentString(extraArgs)
-        true
-      }
-    }
-    val driver = new MainClass
-
-    driver.process(compilerArgs)
-    assert(!driver.reporter.hasErrors)
+    dotty.tools.dotc.Main.process(args)
   }
 
   private var tempDir: File = null
